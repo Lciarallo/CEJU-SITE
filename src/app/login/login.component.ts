3faa74224ec085d53181fa6ebcd634b41c56aa5b
Lciarallo <luizeduardociarallo@gmail.com>
2024-06-03 11:08:45 -0300

    logindev

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'] // Corrigido styleUrl para styleUrls
 })
 export class LoginComponent {
+  textLogin = new FormControl('');
+  textSenha = new FormControl('');
 
+  constructor() {
+    this.textLogin.valueChanges.subscribe(value => {
+      console.log('Login: ', value);
+    });
+    this.textSenha.valueChanges.subscribe(value => {
+      console.log('Senha: ', value);
+    });
+    
+  }
+
+  salvar(): void {
+
+    const data = {
+        login: this.textLogin.value,
+        senha: this.textSenha.value
+    }
+    console.log(data);
+}
 }
