82d044fbd493b8a64f8729e3f44bb15cac6dc026
Lciarallo <85714365+Lciarallo@users.noreply.github.com>
2024-06-07 10:25:00 -0300

    BuscaForm

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,16 @@ import { AppComponent } from './app/app.component';
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('myForm') as HTMLFormElement;
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const search = (document.getElementById('search') as HTMLInputElement).value;
+
+    console.log('Pesquisa:', search);
+
+
+  });
+});
