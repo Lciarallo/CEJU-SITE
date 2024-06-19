3ff655d8290bc13055097844136f76c6bfc8bd65
luisdk2000 <luis.dk2000@aluno.ifsc.edu.br>
2024-06-19 13:53:26 -0300

    ...

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { FeedComponent } from "../feed/feed.component";
 import { FooterComponent } from "../footer/footer.component";
 import { MapaInfoComponent } from '../mapa-info/mapa-info.component';
+import { NoticiaComponent } from '../noticia/noticia.component';
 
 @Component({
     selector: 'app-banner',
     standalone: true,
     templateUrl: './banner.component.html',
     styleUrl: './banner.component.css',
-    imports: [FeedComponent, FooterComponent, MapaInfoComponent]
+    imports: [FeedComponent, FooterComponent, MapaInfoComponent, NoticiaComponent]
 })
 export class BannerComponent {
 
