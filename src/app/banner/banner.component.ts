76935d031fafbdcb979dee848f4f6793667390fc
Lciarallo <85714365+Lciarallo@users.noreply.github.com>
2024-06-19 11:11:53 -0300

    Inicio

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { FeedComponent } from "../feed/feed.component";
 import { FooterComponent } from "../footer/footer.component";
+import { MapaInfoComponent } from '../mapa-info/mapa-info.component';
 
 @Component({
     selector: 'app-banner',
     standalone: true,
     templateUrl: './banner.component.html',
     styleUrl: './banner.component.css',
-    imports: [FeedComponent, FooterComponent]
+    imports: [FeedComponent, FooterComponent, MapaInfoComponent]
 })
 export class BannerComponent {
 
---
+++ src/app/mapa-info/mapa-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mapa-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './mapa-info.component.html',
+  styleUrl: './mapa-info.component.css'
+})
+export class MapaInfoComponent {
+
+}
