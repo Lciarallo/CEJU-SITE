9b76a7e8426142f067894bfa25c03fb02ec9e3d7
Lciarallo <85714365+Lciarallo@users.noreply.github.com>
2024-06-20 09:47:46 -0300

    quem-somos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { BannerComponent } from './banner/banner.component';
 import { ContatoComponent } from './contato/contato.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
 
 export const routes: Routes = [
     {path: '', component: BannerComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'contato', component: ContatoComponent}
+    {path: 'contato', component: ContatoComponent},
+    {path: 'quem-somos', component: QuemSomosComponent}
 ];
